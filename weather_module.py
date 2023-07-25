eea82a056a4717cc297f0138381df9c8efc7391e
JonoDaMidget <99226116+JonoDaMidget@users.noreply.github.com>
2023-07-25 12:58:48 +1200

    Update weather_module.py
    Added location permissions

--- weather_module.py
+++ weather_module.py
@@ -1,17 +1,27 @@
 import geocoder as gc
 from geopy.geocoders import Nominatim
 import pyowm
-import tkinter as tk
+from tkinter import Tk
+from tkinter.messagebox import askyesno
 
-g = gc.ip('me')
-location = Nominatim(user_agent='GetLoc')
-locationName = location.reverse(g.latlng)
-location = locationName.address
-locationlist = location.split(sep = ', ')
-owm = pyowm.OWM('API KEY')
-weather_mgr = owm.weather_manager()
-observation = weather_mgr.weather_at_place(locationlist[-3])
-temperature = observation.weather.temperature('celsius')['temp']
-humidity = observation.weather.humidity
-wind = observation.weather.wind()
-weather = observation.weather.status
+root = Tk()
+root.withdraw()
+allowloc = askyesno('Permissions Required', 
+    'CBT would like to access your location.\nGrant access to this application to access weather functions')
+
+if allowloc == True:
+    g = gc.ip('me')
+    location = Nominatim(user_agent='GetLoc')
+    locationName = location.reverse(g.latlng)
+    location = locationName.address
+    locationlist = location.split(sep = ', ')
+    owm = pyowm.OWM('c47ab2899ea8aef42f862b1dc30c0678')
+    weather_mgr = owm.weather_manager()
+    observation = weather_mgr.weather_at_place(locationlist[-3])
+    temperature = observation.weather.temperature('celsius')['temp']
+    humidity = observation.weather.humidity
+    wind = observation.weather.wind()
+    weather = observation.weather.status
+    root.destroy()
+else:
+    root.destroy()
